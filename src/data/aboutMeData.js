f449e5047954c633887ea9dbe69a2b8b71e5170f
dkklimo <dkklimo@gmail.com>
2023-08-31 12:54:38 +0300

    Changes

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'Passionate Fullstack Software Engineer with 3 years of experience in developing web applications using a diverse skill set encompassing Javascript, ReactJS, NodeJS, PHP, Laravel, MERN Stack, and expertise in WordPress.',
+	},
+	{
+		id: 2,
+		bio: 'With an unwavering attention to detail, I am dedicated to creating transformative solutions that make a positive impact and bring meaningful change to the world. As an enthusiastic problem solver and lover of technology, I am driven to craft innovative and user-centric applications that push the boundaries of what is possible in the digital realm.',
+	},
+];
